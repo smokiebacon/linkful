5e948aaeb8a4ddbbf5b5020d3188454f51c22476
Abraham <smokiebacon@gmail.com>
2024-07-08 01:18:00 -0700

    form

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,24 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section>
-        <h1 className="text-xl bold">Your one link for everything</h1>
-        <h2>
+      <section className="p-4 text-center mx-auto max-w-lg">
+        <h1 className="text-4xl font-bold">Your one link for everything</h1>
+        <h2 className="text-slate-500 text-xl mt-6">
           Share your links, social profiles, contact info, or anything on one
           page.
         </h2>
-      </section>{" "}
+        <form className="text-center" type="text">
+          <span className="px-2 ">linkful.com/</span>
+          <input
+            className="border-500 py-4"
+            type="text"
+            placeholder="username"
+          ></input>
+          <button className="bg-blue-500 text-white py-4 px-6" type="submit">
+            Join Now
+          </button>
+        </form>
+      </section>
     </main>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Header() {
   return (
     <header className="bg-white border-b flex justify-between p-5">
-      <div className="flex gap-4">
+      <div className="flex gap-5">
         <Link href={"/"}>Linkful</Link>
         <nav className="flex gap-10 text-slate-500">
           <Link href={"/about"}>About</Link>
@@ -12,6 +12,16 @@ export default function Header() {
         </nav>
       </div>
       <nav className="flex gap-5 text-slate-500 text-lg">
+        <form>
+          <select
+            id="countries"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Choose Language</option>
+            <option value="US">English</option>
+            <option value="CN">Chinese</option>
+          </select>
+        </form>
         <Link href={"/signin"}>Sign In</Link>
         <Link href={"/register"}>Register</Link>
       </nav>
